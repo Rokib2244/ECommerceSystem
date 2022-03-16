b2f9cd1134f5141ce1be57b05b5da497171e1640
Mohammad Rokibul Hoque <rokib2244@gmail.com>
2022-03-17 00:30:09 +0600

    Code Added

--- ECommerceSystem/ECommerceSystem.Api/Controllers/ProductController.cs
+++ ECommerceSystem/ECommerceSystem.Api/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 namespace ECommerceSystem.Api.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    [ApiController, Authorize(Policy = "AccessPermission")]
     public class ProductController : ControllerBase
     {
         // GET: api/<ValuesController>
